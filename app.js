4a4cf8e908455331171465dfb5d3038448faa79f
Karanbhangu <birk4190@gmail.com>
2020-12-19 15:41:44 +0530

    first commit

--- app.js
+++ app.js
@@ -20,3 +20,89 @@ function calculate(){
     }
 
 }
+function darkmode(){
+    let light = document.getElementById('light');
+    if(light.className == "bodylight"){
+        light.className = "bodydark";
+    }
+    else if(light.className == "bodydark"){
+        light.className = "bodylight"
+    }
+
+    // Headings:
+    let heading = document.getElementById('heading1');
+    if(heading.className == "h3light"){
+        heading.className = "h3dark";
+    }
+    else if(heading.className == "h3dark"){
+        heading.className = "h3light"
+    }
+    let heading2 = document.getElementById('heading2');
+    if(heading2.className == "h3light"){
+        heading2.className = "h3dark";
+    }
+    else if(heading2.className == "h3dark"){
+        heading2.className = "h3light"
+    }
+    let heading3 = document.getElementById('heading3');
+    if(heading3.className == "h3light"){
+        heading3.className = "h3dark";
+    }
+    else if(heading3.className == "h3dark"){
+        heading3.className = "h3light";
+    }
+    // Main div:
+    let maindiv = document.getElementById('maindiv');
+    if(maindiv.className == "contained"){
+        maindiv.className = "contained2";
+    }
+    else if (maindiv.className == "contained2"){
+        maindiv.className = "contained";
+    }
+
+    // Operators:
+    let firstop = document.getElementById("firstop")
+    let secondtop = document.getElementById("secondop")
+    let thirdtop = document.getElementById("thirdop")
+    let fourthop = document.getElementById("fourthop")
+    if(firstop.className == "nones"){
+        firstop.className = "darkb";
+    }
+    else if (firstop.className == "darkb"){
+        firstop.className = "nones";
+    }
+    if(secondtop.className == "nones"){
+        secondtop.className = "darkb";
+    }
+    else if (secondtop.className == "darkb"){
+        secondtop.className = "nones";
+    }
+    if(thirdtop.className == "nones"){
+        thirdtop.className = "darkb";
+    }
+    else if (thirdtop.className == "darkb"){
+        thirdtop.className = "nones";
+    }
+    if(fourthop.className == "nones"){
+        fourthop.className = "darkb";
+    }
+    else if (fourthop.className == "darkb"){
+        fourthop.className = "nones";
+    }   
+    // Buttons:
+    let calcb = document.getElementById("calcb");
+    if(calcb.className=="first"){
+        calcb.className = "calcybut";
+    }
+    else if (calcb.className == "calcybut"){
+        calcb.className = "first";
+    }
+    // span:
+    let spaneed = document.getElementById('spaneed');
+    if(spaneed.className=="noned"){
+        spaneed.className = "spanny";
+    }
+    else if (spaneed.className == "spanny"){
+        spaneed.className = "noned";
+    }
+}
